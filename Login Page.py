d7bd582541666758f3a8d77c221981903ef837ec
vishnubabalsure <vishnubabalsure114@yahoo.com>
2025-10-04 23:17:04 +0530

    Login Page.py

---
+++ Login Page.py
@@ -0,0 +1,12 @@
+print("login".center(50)) 
+
+
+username=input("enter username:")
+password=input("enter password:")
+
+corret_username="thorat"
+corret_password="thorat123"
+if username==corret_username and password==corret_password:
+    print("successfully enter")
+else:
+    print(" not enter")
